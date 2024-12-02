5ecd78fa258287f0a14e6ddb38a22cd99a4ebb64
Dennis <me@dnsc.io>
2024-12-02 22:55:01 +0100

    Updates config

--- custom-vars.el
+++ custom-vars.el
@@ -4,15 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d317d3dacbd7b00340d86dd00c3353bed527c722c4b034fed580d6cdafac0d96"
-	 "7a3f3282c4ce1edc3387deb6f72ed31026ab7dfdf4c25ddcfad9008d2a5a5574"
-	 default)))
+	 '("a75aff58f0d5bbf230e5d1a02169ac2fbf45c930f816f3a21563304d5140d245" "d317d3dacbd7b00340d86dd00c3353bed527c722c4b034fed580d6cdafac0d96" "7a3f3282c4ce1edc3387deb6f72ed31026ab7dfdf4c25ddcfad9008d2a5a5574" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t (:background nil))))
  '(evil-goggles-change-face ((t (:inherit diff-removed))))
  '(evil-goggles-delete-face ((t (:inherit diff-removed))))
  '(evil-goggles-paste-face ((t (:inherit diff-added))))
@@ -21,4 +18,7 @@
  '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
  '(evil-goggles-yank-face ((t (:inherit diff-changed))))
  '(font-lock-comment-face ((t (:inherit shadow))))
- '(shadow ((t (:foreground "#6c7086" :slant italic)))))
+ '(fringe ((t (:background nil))))
+ '(shadow ((t (:foreground "#6c7086" :slant italic))))
+ '(tab-bar ((t (:height 0.85))))
+ '(tab-bar-tab-inactive ((t (:slant italic :foreground "#606270")))))
--- init.el
+++ init.el
@@ -426,10 +426,9 @@
   (project-vc-ignores '("target/" "bin/" "out/" "node_modules/"))
   (project-vc-extra-root-markers '(".project" "package.json" "Cargo.toml" "go.mod" "Gemfile")))
 
-(use-package emacs
-  :ensure nil
+(use-package modus-themes
+	:ensure t
   :config
-  (require-theme 'modus-themes)
 
   (custom-set-faces
    '(tab-bar ((t (:height 0.85))))
@@ -514,61 +513,6 @@
   :after vertico
   :init
   (marginalia-mode))
-
-;; Corfu setup
-(use-package corfu
-  ;; Optional customizations
-  :custom
-  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  (corfu-auto t)                 ;; Enable auto completion
-  (corfu-auto-prefix 2)          ;; Minimum length of prefix for auto completion.
-  (corfu-popupinfo-mode t)       ;; Enable popup information
-  (corfu-popupinfo-delay 0.5)    ;; Lower popupinfo delay to 0.5 seconds from 2 seconds
-  (corfu-separator ?\s)          ;; Orderless field separator, Use M-SPC to enter separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-  (completion-ignore-case t)
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (tab-always-indent 'complete)
-  (corfu-preview-current nil) ;; Don't insert completion without confirmation
-  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
-  ;; be used globally (M-/).  See also the customization variable
-  ;; `global-corfu-modes' to exclude certain modes.
-  :init
-  (global-corfu-mode))
-
-(use-package nerd-icons-corfu
-  :after corfu
-  :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
-
-(use-package cape
-  :after corfu
-  :init
-  ;; Add to the global default value of `completion-at-point-functions' which is
-  ;; used by `completion-at-point'.  The order of the functions matters, the
-  ;; first function returning a result wins.  Note that the list of buffer-local
-  ;; completion functions takes precedence over the global list.
-  ;; The functions that are added later will be the first in the list
-
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev) ;; Complete word from current buffers
-  (add-to-list 'completion-at-point-functions #'cape-dict) ;; Dictionary completion
-  (add-to-list 'completion-at-point-functions #'cape-file) ;; Path completion
-  (add-to-list 'completion-at-point-functions #'cape-elisp-block) ;; Complete elisp in Org or Markdown mode
-  (add-to-list 'completion-at-point-functions #'cape-keyword) ;; Keyword/Snipet completion
-
-  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev) ;; Complete abbreviation
-  ;;(add-to-list 'completion-at-point-functions #'cape-history) ;; Complete from Eshell, Comint or minibuffer history
-  ;;(add-to-list 'completion-at-point-functions #'cape-line) ;; Complete entire line from current buffer
-  ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol) ;; Complete Elisp symbol
-  ;;(add-to-list 'completion-at-point-functions #'cape-tex) ;; Complete Unicode char from TeX command, e.g. \hbar
-  ;;(add-to-list 'completion-at-point-functions #'cape-sgml) ;; Complete Unicode char from SGML entity, e.g., &alpha
-  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345) ;; Complete Unicode char using RFC 1345 mnemonics
-  )
 
 (use-package consult
   ;; Enable automatic preview at point in the *Completions* buffer. This is
@@ -748,6 +692,30 @@
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
+(use-package lsp-mode
+	:custom
+	(lsp-keymap-prefix "C-c l")
+	(lsp-use-plists t)
+	(lsp-log-io nil)
+	(lsp-auto-configure t)
+  (lsp-enable-suggest-server-download t)
+	(lsp-completion-enable t)
+	(lsp-completion-show-kind t)
+	(lsp-enable-file-watchers nil)
+  (lsp-enable-folding nil)
+	(lsp-enable-indentation nil)
+  (lsp-enable-on-type-formatting nil)
+  (lsp-enable-symbol-highlighting nil)
+  (lsp-enable-text-document-color nil) 
+	(lsp-enable-snippet nil)
+	(lsp-semantic-tokens-enable nil)
+	(lsp-headerline-breadcrumb-enable-diagnostics nil)
+  :init
+  :hook (
+         (clojure-ts-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+	:commands lsp)
+
 (use-package add-node-modules-path
   :ensure t
   :defer t
@@ -765,6 +733,11 @@
 	:ensure nil
 	:custom
 	(typescript-ts-mode-indent-offset 2))
+
+(use-package clojure-ts-mode
+	:ensure t
+	:custom
+	(clojure-ts-comment-macro-font-lock-body t))
 
 (use-package jinx
   :hook (emacs-startup . global-jinx-mode))
