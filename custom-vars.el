c54ed0e8e68c095725ba132606b4c2cbb4867774
Dennis <me@dnsc.io>
2025-09-07 22:41:13 +0200

    feat: updates tab bar styling

--- custom-vars.el
+++ custom-vars.el
@@ -4,14 +4,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("6bf350570e023cd6e5b4337a6571c0325cec3f575963ac7de6832803df4d210a" "a75aff58f0d5bbf230e5d1a02169ac2fbf45c930f816f3a21563304d5140d245" "d317d3dacbd7b00340d86dd00c3353bed527c722c4b034fed580d6cdafac0d96" "7a3f3282c4ce1edc3387deb6f72ed31026ab7dfdf4c25ddcfad9008d2a5a5574" default))
+   '("6bf350570e023cd6e5b4337a6571c0325cec3f575963ac7de6832803df4d210a"
+     "a75aff58f0d5bbf230e5d1a02169ac2fbf45c930f816f3a21563304d5140d245"
+     "d317d3dacbd7b00340d86dd00c3353bed527c722c4b034fed580d6cdafac0d96"
+     "7a3f3282c4ce1edc3387deb6f72ed31026ab7dfdf4c25ddcfad9008d2a5a5574" default))
  '(package-selected-packages
-   '(gptel apheleia lsp-tailwindcss astro-ts-mode add-node-modules-path clojure-ts-mode jinx alert org-alert consult-denote denote visual-fill-column org-modern git-timemachine diff-hl cape nerd-icons-corfu corfu mood-line modus-themes popper activities general vundo undo-fu-session undo-fu diredfl all-the-icons-dired all-the-icons dired-narrow exec-path-from-shell)))
+   '(gptel apheleia lsp-tailwindcss astro-ts-mode add-node-modules-path clojure-ts-mode jinx alert
+           org-alert consult-denote denote visual-fill-column org-modern git-timemachine diff-hl
+           cape nerd-icons-corfu corfu mood-line modus-themes popper activities general vundo
+           undo-fu-session undo-fu diredfl all-the-icons-dired all-the-icons dired-narrow
+           exec-path-from-shell))
+ '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(activities-tabs ((t (:slant italic :foreground "#989898"))))
  '(evil-goggles-change-face ((t (:inherit diff-removed))))
  '(evil-goggles-delete-face ((t (:inherit diff-removed))))
  '(evil-goggles-paste-face ((t (:inherit diff-added))))
